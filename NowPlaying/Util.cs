b07af8ff29976415d3fe8b414bd556b242388bb6
Joel Low <joel@joelsplace.sg>
2011-08-30 03:10:18 +0000

    Split up the giant MainInterface file.

---
+++ NowPlaying/Util.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoelLow.NowPlaying
+{
+	class Util
+	{
+		public static List<IntPtr> FindWindows(string wndclass, string title)
+		{
+			NativeMethods.WindowSearchData sd = new NativeMethods.WindowSearchData {
+				Wndclass = wndclass,
+				Title = title
+			};
+
+			NativeMethods.EnumWindows(EnumProc, ref sd);
+			return sd.hWnds;
+		}
+
+		private static bool EnumProc(IntPtr hWnd, ref NativeMethods.WindowSearchData data)
+		{
+			StringBuilder sb = new StringBuilder(1024);
+			NativeMethods.GetClassName(hWnd, sb, sb.Capacity);
+
+			if (data.Wndclass == null || sb.ToString().ToUpperInvariant() == data.Wndclass.ToUpperInvariant())
+			{
+				sb = new StringBuilder(1024);
+				NativeMethods.GetWindowText(hWnd, sb, sb.Capacity);
+				if (data.Title == null || sb.ToString().ToUpperInvariant() == data.Title.ToUpperInvariant())
+				{
+					data.hWnds.Add(hWnd);
+				}
+			}
+
+			return true;
+		}
+	}
+}
