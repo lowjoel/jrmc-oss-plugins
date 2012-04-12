31982e5f18c0b899baf6551f87024ae3a1ad7b16
Joel <Joel@ff59d194-c0fc-4472-7f31-d9db512032c5>
2012-04-12 05:09:38 +0000

    Write the marshalling code for MC's unmanaged data to FlacCL's managed bits.

--- trunk/MediaCenterFlacCLEncoder/MediaCenterFlacCLEncoder.h
+++ trunk/MediaCenterFlacCLEncoder/MediaCenterFlacCLEncoder.h
@@ -7,6 +7,9 @@ namespace MediaCenterFlacCLEncoder {
 		MediaCenterFlacCLEncoder(System::String^ outPath,
 			CUETools::Codecs::AudioPCMConfig^ format);
 
+		void Write(array<System::Byte>^ data);
+		void Finish();
+
 		static System::Reflection::Assembly^ LoadDependencies(System::Object^ sender,
 			System::ResolveEventArgs^ args);
 
