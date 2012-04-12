5d042c70074716d63dfb0257c0edaa9e66823b6c
Joel Low <joel@joelsplace.sg>
2012-04-12 01:08:55 +0000

    Implemented encoder registration.

--- MediaCenterFlacCLEncoder/JREncoderInterface.cpp
+++ MediaCenterFlacCLEncoder/JREncoderInterface.cpp
@@ -66,7 +66,41 @@ namespace MediaCenterFlacCLEncoder {
 	extern "C" __declspec(dllexport)
 	HRESULT __stdcall DllRegisterServer(void)
 	{
-		return SELFREG_E_CLASS;
+		HKEY key = { 0 };
+		if (RegCreateKeyEx(HKEY_LOCAL_MACHINE,
+			L"SOFTWARE\J. River\Media Jukebox\Plugins\Encoders\FlacCL Encoder",
+			0, nullptr, REG_OPTION_NON_VOLATILE, KEY_WRITE | KEY_READ, nullptr,
+			&key, nullptr) != ERROR_SUCCESS)
+		{
+			return SELFREG_E_CLASS;
+		}
+
+		auto RegSetStringValue = [](HKEY key, const wchar_t* valueName, const wchar_t* valueValue)
+		{
+			if (RegSetValueEx(key, valueName, 0, REG_SZ, reinterpret_cast<const BYTE*>(valueValue),
+				sizeof(*valueValue) * (wcslen(valueValue) + 1)) != ERROR_SUCCESS)
+			{
+				throw GetLastError();
+			}
+		};
+
+		HRESULT returnCode = SELFREG_E_CLASS;
+		try
+		{
+			RegSetStringValue(key, L"Company", L"Joel Low");
+			RegSetStringValue(key, L"Copyright", L"Copyright");
+			RegSetStringValue(key, L"Description", L"FlacCL Encoder Plugin for Media Jukebox/Media Center");
+			RegSetStringValue(key, L"Ext", L"flac");
+			RegSetStringValue(key, L"URL", L"http://joelsplace.sg");
+			RegSetStringValue(key, L"Version", L"1.0.0");
+			returnCode = S_OK;
+		}
+		catch (DWORD error)
+		{
+		}
+
+		RegCloseKey(key);
+		return returnCode;
 	}
 
 	extern "C" __declspec(dllexport)
@@ -74,7 +108,5 @@ namespace MediaCenterFlacCLEncoder {
 	{
 		return S_FALSE;
 	}
-
-
 #pragma endregion
 }
