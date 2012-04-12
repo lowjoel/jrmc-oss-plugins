dc4261c0c02fc319e53156e310fbfd2796bba0ce
Joel <Joel@ff59d194-c0fc-4472-7f31-d9db512032c5>
2012-04-12 00:25:12 +0000

    Added the FlacCL encoder project and rename the Now Playing project for grouping.

---
+++ trunk/MediaCenterFlacCLEncoder/MediaCenterFlacCLEncoder.h
@@ -0,0 +1,13 @@
+// MediaCenterFlacCLEncoder.h
+
+#pragma once
+
+using namespace System;
+
+namespace MediaCenterFlacCLEncoder {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
