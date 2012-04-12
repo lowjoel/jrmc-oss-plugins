cddeeb4c84c27c1f209f4b302f8c47610612000c
Joel <Joel@ff59d194-c0fc-4472-7f31-d9db512032c5>
2012-04-12 08:19:47 +0000

    Forgot to initialise some integer values. Also, for now, we can just cast our memory representation to a string and store it. The storage of the string seems to be binary-safe.

--- trunk/MediaCenterFlacCLEncoder/JREncoderInterfaceConfig.cpp
+++ trunk/MediaCenterFlacCLEncoder/JREncoderInterfaceConfig.cpp
@@ -12,6 +12,15 @@ namespace MediaCenterFlacCLEncoder {
 		OffloadTasksToCpu = false;
 		DoRiceEncoding = false;
 
+		GpuWorkGroupSize = -1;
+		FramesPerMultiprocessor = -1;
+		AdditionalCpuThreads = -1;
+		BlockSize = -1;
+		TasksPerChannel = -1;
+		TasksPerWindow = -1;
+		StereoDecorrelationAlgorithm = -1;
+		WindowAlgorithm = -1;
+
 		PartitionOrder = std::make_pair(-1, -1);
 		PredictionOrder = std::make_pair(-1, -1);
 		FixedOrder = std::make_pair(-1, -1);
@@ -22,10 +31,19 @@ namespace MediaCenterFlacCLEncoder {
 
 	MediaCenterFlacCLEncoderInterface::Config::Config(const std::wstring& str)
 	{
+		if (str.length() * sizeof(wchar_t) >= sizeof(*this))
+		{
+			*this = *reinterpret_cast<const Config*>(&str[0]);
+		}
+		else
+		{
+			*this = Config();
+		}
 	}
 
 	MediaCenterFlacCLEncoderInterface::Config::operator std::wstring() const
 	{
-		return std::wstring();
+		return std::wstring(reinterpret_cast<const wchar_t*>(this),
+			sizeof(*this) / sizeof(wchar_t));
 	}
 }
