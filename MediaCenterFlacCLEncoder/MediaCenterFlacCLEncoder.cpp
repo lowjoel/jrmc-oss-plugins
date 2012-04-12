b59c1f2276d9e00aedddb1bad9cfe154901fe8f3
Joel Low <joel@joelsplace.sg>
2012-04-12 04:23:51 +0000

    When initialising our Plugin, we need to add an assembly search path to the directory of the current DLL since we will need to load the FlacCL assemblies. Do this so that the encoder can load properly.

--- MediaCenterFlacCLEncoder/MediaCenterFlacCLEncoder.cpp
+++ MediaCenterFlacCLEncoder/MediaCenterFlacCLEncoder.cpp
@@ -4,3 +4,30 @@
 
 #include "MediaCenterFlacCLEncoder.h"
 
+using namespace System;
+using namespace System::IO;
+using namespace System::Reflection;
+using namespace CUETools::Codecs::FLACCL;
+
+namespace MediaCenterFlacCLEncoder {
+	MediaCenterFlacCLEncoder::MediaCenterFlacCLEncoder(String^ outPath)
+	{
+		Writer = nullptr;/*gcnew FLACCLWriter((output_file == "-" || output_file == "nul") ? "" : output_file,
+				output_file == "-" ? Console.OpenStandardOutput() :
+				output_file == "nul" ? new NullStream() : null,
+				audioSource.PCM);
+			encoder.FinalSampleCount = audioSource.Length;*/
+		Writer->FinalSampleCount = 0;
+	}
+
+	Assembly^ MediaCenterFlacCLEncoder::LoadDependencies(Object^ sender, ResolveEventArgs^ args)
+	{
+		String^ folderPath = Path::GetDirectoryName(Assembly::GetExecutingAssembly()->Location);
+		String^ assemblyPath = Path::Combine(folderPath, (gcnew AssemblyName(args->Name))->Name + ".dll");
+		if (File::Exists(assemblyPath) == false)
+			return nullptr;
+
+		Assembly^ assembly = Assembly::LoadFrom(assemblyPath);
+		return assembly;
+	}
+}
